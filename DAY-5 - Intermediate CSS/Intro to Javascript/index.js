552321ab6b36562d13944933bd57887bcb68c583
andrewch82 <andrewch82@gmail.com>
2024-03-25 22:01:04 -0400

    Day 5 files - CSS & Javascript

---
+++ DAY-5 - Intermediate CSS/Intro to Javascript/index.js
@@ -0,0 +1,13 @@
+let firstName = "John";
+let FirstName = "Jane";
+
+console.log("Hello!", firstName)
+console.log("Hello!", FirstName)
+
+
+// Guideline for creating variable names:
+// 1.  Should be meaningful and descriptive
+// 2.  Should not start with a number
+// 3.  Should not contain a space or hyphen
+// 4.  Case-Sensitive
+// 5.  Cannot use reserved keywords
